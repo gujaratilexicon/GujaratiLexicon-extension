0e44e75af1b063f99ac73f4a82fa6d2a3c3655ae
Apoorva <apoorva.paneliya@gmail.com>
2012-11-24 21:19:11 +0530

    Dictionary Option

---
+++ background.js
@@ -0,0 +1,24 @@
+function getDictionaryOption(){
+	return $('#dict').text(); 
+}
+
+function setDictonayOption(option){
+	$('#dict').text(option);
+}
+
+
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+
+  	if(request.getDictOpt)
+  	{
+  		sendResponse({ curDictOpt: getDictionaryOption() });
+  	}
+  	else if(request.setDictOpt)
+  	{
+  		setDictonayOption(request.setDictOpt);
+  	}
+
+
+
+  });
