8feac66711c6d608a50bc3257e03342fdbeead1b
Apoorva <apoorva.paneliya@gmail.com>
2012-11-25 18:07:06 +0530

    composeURL function added

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+
+
 function getDictionaryOption(){
 	return $('#dict').text(); 
 }
@@ -6,6 +8,51 @@ function setDictonayOption(option){
 	$('#dict').text(option);
 }
 
+function XmlHTTPRequest(URL, callback){
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", URL, false);
+
+    xhr.onreadystatechange = function() {
+    
+    
+     if (xhr.readyState == 4 && xhr.status==200) {
+      //document.getElementById("add").innerHTML =  xhr.responseText;
+        
+        callback(xhr.responseText); 
+        
+   
+      } 
+
+     
+  }
+    
+    
+xhr.send();   
+
+    
+}
+
+function composeURL( dictType, selectedWord ){
+
+  if(dictType === 0)
+  {
+    return "http://www.gujaratilexicon.com/dictionary/EG/" + selectedWord;
+  }
+  else if(dictType === 1)
+  {
+    return "http://www.gujaratilexicon.com/dictionary/GG/" + selectedWord;
+  }
+  else if(dictType === 2)
+  {
+    return "http://www.gujaratilexicon.com/dictionary/GE/" + selectedWord;
+  }
+  else if(dictType === 3)
+  {
+    return "http://www.gujaratilexicon.com/dictionary/HG/" + selectedWord;
+  }
+
+}
 
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -20,7 +67,18 @@ chrome.extension.onMessage.addListener(
       sendResponse({});
   	}
 
-    
+    if(request.SW)
+    {
+      var tempDictAndWord = request.SW;
+      var dictionaryType = tempDictAndWord[0];
+      var tempSelectedWord = tempDictAndWord.substr(1 , tempDictAndWord.length);
+
+      var URL = composeURL(dictionaryType , tempSelectedWord);
+
+
+
+    }
+
 
 
 
