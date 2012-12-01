36c047648b4af6f6e531c946070ff3c5f9d0868e
Apoorva <apoorva.paneliya@gmail.com>
2012-12-01 20:09:18 +0530

    wordlanguageIndetification function added

--- SelectedTextnotification.js
+++ SelectedTextnotification.js
@@ -13,7 +13,7 @@ var GujaratiLexicon = {
 
 	selectedWordNotification: function(){
 		this.selectedWord = window.getSelection().toString().trim();
-		
+		this.wordLangIndentification(this.selectedWord);
 	},
 
 	handelKeyPress:function(event){
@@ -24,8 +24,34 @@ var GujaratiLexicon = {
 		}
 
 	},
-	
+
 	removeDictBubble:function(){
+
+	},
+
+	wordLangIndentification : function( tempWord ){
+
+		if(tempWord === "")
+		{
+			return false;
+		}
+
+		if(/^[a-zA-Z]*$/.test(tempWord)) //test for English Language
+		{
+			return 0; 
+		}
+		else if( /^[\u0A80-\u0AFF]*$/.test(tempWord) ) //test for Gujarati Language
+		{
+			return 1;
+		}
+		else if( /^[\u0900-\u097F]*$/.test(tempWord) ) //test for Hindi Language
+		{
+			return 2;
+		}
+		else
+		{
+			return false;
+		}
 
 	}
 
