d09cf098278dfd13051866ad2d5c3889d46b7016
Apoorva <apoorva.paneliya@gmail.com>
2012-12-01 19:37:36 +0530

    Word Selection Completed

--- SelectedTextnotification.js
+++ SelectedTextnotification.js
@@ -1,17 +1,36 @@
 var GujaratiLexicon = {
 
+	selectedWord:'',
+
 	init:function(){
 
 		//Intiliaze Event Handler
-		document.onkeydown = handelKeyPress;
-		document.body.onclick = removeDictionary;
-		document.body.addEventListener('dblclick',this.selectWord,false);
+		document.onkeydown = this.handelKeyPress;
+		document.body.onclick = this.removeDictBubble;
+		document.body.addEventListener('dblclick',this.selectedWordNotification,false);
+
+	},
+
+	selectedWordNotification: function(){
+		this.selectedWord = window.getSelection().toString().trim();
+		
+	},
+
+	handelKeyPress:function(event){
+
+		if(event.keyCode == 27)
+		{
+	 		this.removeDictBubble();	
+		}
+
+	},
+	
+	removeDictBubble:function(){
 
 	}
 
-
-
-
 };
+
+
 
 GujaratiLexicon.init();
